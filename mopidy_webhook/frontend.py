06f6b80d439e7eee99b9541fdd85babab3801989
mark <mark@rehabstudio.com>
2014-08-22 17:13:49 +0100

    inital track management

--- mopidy_webhook/frontend.py
+++ mopidy_webhook/frontend.py
@@ -16,6 +16,27 @@ from mopidy.models import ModelJSONEncoder
 logger = logging.getLogger(__name__)
 
 
+def _fetch_head_track_uri(webhook_url):
+    response = requests.get(webhook_url)
+    queue_track = response.json()
+    track = queue_track['track']
+
+    if (track['source_type'] == 'spotify'):
+        track_uri = 'spotify:track:' + track['source_id']
+    elif (track['source_type'] == 'soundcloud'):
+        track_uri = 'soundcloud:song/' + track['name'] + '.' + track['source_id']
+    elif (track['source_type'] == 'youtube'):
+        track_uri = 'youtube:video/' + track['name'] + '.' + track['source_id']
+    else:
+        return None
+    return track_uri
+
+
+def _pop_head(webhook_url):
+    logger.info('pop head called')
+    return requests.delete(webhook_url)
+
+
 def _send_webhook(class_name, webhook_url, event=None, **kwargs):
     logger.info('{0} Webhook URL: {1}'.format(class_name, webhook_url))
 
@@ -23,7 +44,8 @@ def _send_webhook(class_name, webhook_url, event=None, **kwargs):
     logger.info('{0} Webhook Payload: {1}'.format(class_name, payload))
 
     try:
-        response = requests.post(webhook_url, data=payload)
+        #response = requests.post(webhook_url, data=payload)
+        raise
     except Exception as e:
         logger.warning('Unable to send {0} Webhook: ({1}) {2}'.format(
             class_name,
@@ -79,7 +101,56 @@ class EventReporter(pykka.ThreadingActor, CoreListener):
         _send_webhook(self.__class__.__name__, webhook_url, event, **kwargs)
 
 
-class WebhookFrontend(pykka.ThreadingActor):
+class TrackManager(pykka.ThreadingActor, CoreListener):
+
+    def __init__(self, config, core):
+        super(TrackManager, self).__init__()
+        self.core = core
+        self.config = config
+        self.webhook_url = config['webhook']['webhook'] + 'head/'
+
+    def on_start(self):
+        logger.info('{0} actor started.'.format(self.__class__.__name__))
+        # Grab the track at the top of the queue
+        uri = _fetch_head_track_uri(self.webhook_url)
+        # Add track to playlist
+        self.core.tracklist.add(tracks=None, at_position=None, uri=uri)
+        # Start track
+        self.core.playback.play()
+
+    def on_stop(self):
+        logger.info('{0} actor stopped.'.format(self.__class__.__name__))
+
+    def on_event(self, event, **kwargs):
+        logger.info(event)
+        if event == 'playlists_loaded':
+            logger.info('track loaded')
+        if event == 'tracklist_changed':
+            logger.info('track changed')
+        if event == 'track_playback_started':
+            logger.info('track started')
+        if event == 'seeked':
+            logger.info('track seeked')
+        if event == 'track_playback_paused':
+            logger.info('track paused')
+        if event == 'track_playback_resumed':
+            logger.info('track resumed')
+        if event == 'track_playback_ended':
+            logger.info('track ended')
+            response = _pop_head(self.webhook_url + 'pop/')
+            logger.info(response)
+            time.sleep(1)
+            # Clean up tracklist
+            self.core.tracklist.clear()
+            # Grab the new track at the top of the queue
+            uri = _fetch_head_track_uri(self.webhook_url)
+            # Add track to playlist
+            self.core.tracklist.add(tracks=None, at_position=None, uri=uri)
+            # Start track new track
+            self.core.playback.play()
+
+
+class WebhookFrontend(pykka.ThreadingActor, CoreListener):
 
     def __init__(self, config, core):
         super(WebhookFrontend, self).__init__()
@@ -87,17 +158,34 @@ class WebhookFrontend(pykka.ThreadingActor):
         self.core = core
         self.event_reporter = None
         self.status_reporter = None
+        self.webhook_url = config['webhook']['webhook'] + 'head/'
 
     def on_start(self):
-        self.event_reporter = EventReporter.start(self.config, self.core)
-        self.status_reporter = StatusReporter.start(self.config, self.core)
+        #self.event_reporter = EventReporter.start(self.config, self.core)
+        #self.status_reporter = StatusReporter.start(self.config, self.core)
+        self.track_reporter = TrackManager.start(self.config, self.core)
 
     def _stop_children(self):
-        self.event_reporter.stop()
-        self.status_reporter.stop()
+        #self.event_reporter.stop()
+        #self.status_reporter.stop()
+        pass
 
     def on_stop(self):
-        self._stop_children()
+        #self._stop_children()
+        pass
 
     def on_failure(self, exception_type, exception_value, traceback):
-        self._stop_children()
+        #self._stop_children()
+        pass
+
+    def track_playback_started(tl_track):
+        #logger.info('track started')
+        pass
+
+    def track_playback_paused(self, tl_track, time_position):
+        #logger.info('track paused')
+        pass
+
+    def track_playback_ended(self, tl_track, time_position):
+        #logger.info('track ended')
+        pass
