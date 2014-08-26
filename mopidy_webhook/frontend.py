c2c6c5e1eacc183f91b536c41b958b3687f94e47
mark <mark@rehabstudio.com>
2014-08-26 14:26:37 +0100

    token added to all requests to api server

--- mopidy_webhook/frontend.py
+++ mopidy_webhook/frontend.py
@@ -16,15 +16,31 @@ from mopidy.models import ModelJSONEncoder
 logger = logging.getLogger(__name__)
 
 
-def _fetch_head_track_uri(webhook_url):
-    response = requests.get(webhook_url)
+def _fetch_player_data(config):
+    token = config['webhook']['token']
+    webhook_url = config['webhook']['webhook'] + 'players/' + token + '/'
+    headers = {'PLAYER_AUTH_TOKEN': token}
+    response = requests.get(
+        webhook_url,
+        headers=headers
+    )
+    return response.json()
+
+
+def _fetch_head_track_uri(webhook_url, token):
+    headers = {'PLAYER_AUTH_TOKEN': token}
+    response = requests.get(
+        webhook_url,
+        headers=headers
+    )
     queue_track = response.json()
     track = queue_track['track']
 
     if (track['source_type'] == 'spotify'):
         track_uri = 'spotify:track:' + track['source_id']
     elif (track['source_type'] == 'soundcloud'):
-        track_uri = 'soundcloud:song/' + track['name'] + '.' + track['source_id']
+        track_uri = 'soundcloud:song/' + track['name'] + \
+            '.' + track['source_id']
     elif (track['source_type'] == 'youtube'):
         track_uri = 'youtube:video/' + track['name'] + '.' + track['source_id']
     else:
@@ -32,20 +48,34 @@ def _fetch_head_track_uri(webhook_url):
     return track_uri
 
 
-def _pop_head(webhook_url):
+def _pop_head(webhook_url, token):
+    logger.info('token')
+    logger.info(token)
+    headers = {'PLAYER_AUTH_TOKEN': token}
     logger.info('pop head called')
-    return requests.delete(webhook_url)
+    response = requests.delete(
+        webhook_url,
+        headers=headers
+    )
+    return response
 
 
-def _send_webhook(class_name, webhook_url, event=None, **kwargs):
+def _send_webhook(class_name, webhook_url, token, **kwargs):
     logger.info('{0} Webhook URL: {1}'.format(class_name, webhook_url))
 
     payload = json.dumps(kwargs, cls=ModelJSONEncoder, indent=2)
     logger.info('{0} Webhook Payload: {1}'.format(class_name, payload))
 
     try:
-        #response = requests.post(webhook_url, data=payload)
-        raise
+        headers = {
+            'PLAYER_AUTH_TOKEN': token,
+            'content-type': 'application/json'
+        }
+        response = requests.post(
+            webhook_url,
+            data=json.dumps(payload),
+            headers=headers
+        )
     except Exception as e:
         logger.warning('Unable to send {0} Webhook: ({1}) {2}'.format(
             class_name,
@@ -53,8 +83,12 @@ def _send_webhook(class_name, webhook_url, event=None, **kwargs):
             e.message,
         ))
     else:
-        logger.info('{0} Webhook Response Status: {1}'.format(class_name, response.status_code))
-        logger.info('{0} Webhook Response Body: {1}'.format(class_name, response.text))
+        logger.info('{0} Webhook Response Status: {1}'.format(
+            class_name, response.status_code
+        ))
+        logger.info('{0} Webhook Response Body: {1}'.format(
+            class_name, response.text
+        ))
 
 
 class StatusReporter(pykka.ThreadingActor):
@@ -73,13 +107,15 @@ class StatusReporter(pykka.ThreadingActor):
         logger.info('{0} actor stopped.'.format(self.__class__.__name__))
 
     def report_status(self):
-        webhook_url = self.config['webhook']['webhook']
+        token = self.config['webhook']['token']
+        webhook_url = self.config['webhook']['webhook'] + 'players/' + \
+            token + '/status/'
         kwargs = {
             'current_track': self.core.playback.current_track.get(),
             'state': self.core.playback.state.get(),
             'time_position': self.core.playback.time_position.get(),
         }
-        _send_webhook(self.__class__.__name__, webhook_url, **kwargs)
+        _send_webhook(self.__class__.__name__, webhook_url, token, **kwargs)
         time.sleep(2)
         self.in_future.report_status()
 
@@ -97,22 +133,32 @@ class EventReporter(pykka.ThreadingActor, CoreListener):
         logger.info('{0} actor stopped.'.format(self.__class__.__name__))
 
     def on_event(self, event, **kwargs):
-        webhook_url = self.config['webhook']['webhook'] + event + '/'
-        _send_webhook(self.__class__.__name__, webhook_url, event, **kwargs)
+        token = self.config['webhook']['token']
+        webhook_url = self.config['webhook']['webhook'] + 'players/' + \
+            token + '/event/' + event + '/'
+        kwargs = {
+            'event': event,
+        }
+        _send_webhook(self.__class__.__name__, webhook_url, token, **kwargs)
 
 
 class TrackManager(pykka.ThreadingActor, CoreListener):
 
-    def __init__(self, config, core):
+    def __init__(self, config, core, player_data):
         super(TrackManager, self).__init__()
         self.core = core
         self.config = config
-        self.webhook_url = config['webhook']['webhook'] + 'head/'
+        self.player_data = player_data
+        self.webhook_url = config['webhook']['webhook'] + 'queues/' + \
+            str(player_data['queue']) + '/head/'
 
     def on_start(self):
         logger.info('{0} actor started.'.format(self.__class__.__name__))
         # Grab the track at the top of the queue
-        uri = _fetch_head_track_uri(self.webhook_url)
+        uri = _fetch_head_track_uri(
+            self.webhook_url,
+            self.player_data['token']
+        )
         # Add track to playlist
         self.core.tracklist.add(tracks=None, at_position=None, uri=uri)
         # Start track
@@ -137,13 +183,16 @@ class TrackManager(pykka.ThreadingActor, CoreListener):
             logger.info('track resumed')
         if event == 'track_playback_ended':
             logger.info('track ended')
-            response = _pop_head(self.webhook_url + 'pop/')
+            response = _pop_head(self.webhook_url + 'pop/', self.player_data['token'])
             logger.info(response)
-            time.sleep(1)
+            time.sleep(0.5)
             # Clean up tracklist
             self.core.tracklist.clear()
             # Grab the new track at the top of the queue
-            uri = _fetch_head_track_uri(self.webhook_url)
+            uri = _fetch_head_track_uri(
+                self.webhook_url,
+                self.player_data['token']
+            )
             # Add track to playlist
             self.core.tracklist.add(tracks=None, at_position=None, uri=uri)
             # Start track new track
@@ -158,12 +207,16 @@ class WebhookFrontend(pykka.ThreadingActor, CoreListener):
         self.core = core
         self.event_reporter = None
         self.status_reporter = None
-        self.webhook_url = config['webhook']['webhook'] + 'head/'
+        self.player_data = _fetch_player_data(config)
 
     def on_start(self):
         #self.event_reporter = EventReporter.start(self.config, self.core)
         #self.status_reporter = StatusReporter.start(self.config, self.core)
-        self.track_reporter = TrackManager.start(self.config, self.core)
+        self.track_reporter = TrackManager.start(
+            self.config,
+            self.core,
+            self.player_data
+        )
 
     def _stop_children(self):
         #self.event_reporter.stop()
@@ -176,16 +229,4 @@ class WebhookFrontend(pykka.ThreadingActor, CoreListener):
 
     def on_failure(self, exception_type, exception_value, traceback):
         #self._stop_children()
-        pass
-
-    def track_playback_started(tl_track):
-        #logger.info('track started')
-        pass
-
-    def track_playback_paused(self, tl_track, time_position):
-        #logger.info('track paused')
-        pass
-
-    def track_playback_ended(self, tl_track, time_position):
-        #logger.info('track ended')
         pass
