11866d0c18296c482e2d5c67b6d28595adfa8502
mark <mark@rehabstudio.com>
2014-08-27 11:58:50 +0100

    webhooks > patch method added

--- mopidy_webhook/webhooks.py
+++ mopidy_webhook/webhooks.py
@@ -96,6 +96,33 @@ class Webhooks(object):
             ))
         return response.json()
 
+    def patch(self, class_name, webhook_url, **kwargs):
+        logger.info('{0} Webhook URL: {1}'.format(class_name, webhook_url))
+
+        payload = json.dumps(kwargs, cls=ModelJSONEncoder, indent=2)
+        logger.info('{0} Webhook Payload: {1}'.format(class_name, payload))
+
+        try:
+            response = requests.patch(
+                webhook_url,
+                data=json.dumps(payload),
+                headers=self.headers
+            )
+        except Exception as e:
+            logger.warning('Unable to send {0} Webhook: ({1}) {2}'.format(
+                class_name,
+                e.__class__.__name__,
+                e.message,
+            ))
+        else:
+            logger.info('{0} Webhook Response Status: {1}'.format(
+                class_name, response.status_code
+            ))
+            logger.info('{0} Webhook Response Body: {1}'.format(
+                class_name, response.text
+            ))
+        return response.json()
+
     def delete(self, class_name, webhook_url, **kwargs):
         logger.info('{0} Webhook URL: {1}'.format(class_name, webhook_url))
 
