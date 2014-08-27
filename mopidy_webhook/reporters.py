e75ffbe0bee0cd6c338c96150fcd0dacea3dda47
mark <mark@rehabstudio.com>
2014-08-27 10:54:25 +0100

    reporters - cs and refactored

--- mopidy_webhook/reporters.py
+++ mopidy_webhook/reporters.py
@@ -17,13 +17,15 @@ logger = logging.getLogger(__name__)
 
 
 class StatusReporter(pykka.ThreadingActor):
-
+    """Updates the API server with tracklist and playback status.
+    """
     def __init__(self, config, core, player_data):
         super(StatusReporter, self).__init__()
         self.config = config
         self.core = core
         self.player_data = player_data
         self.in_future = self.actor_ref.proxy()
+        self.webhook = Webhooks(config['webhook']['token'])
 
     def on_start(self):
         logger.info('{0} actor started.'.format(self.__class__.__name__))
@@ -31,51 +33,59 @@ class StatusReporter(pykka.ThreadingActor):
 
     def on_stop(self):
         logger.info('{0} actor stopped.'.format(self.__class__.__name__))
-        token = self.config['webhook']['token']
-        webhook = Webhooks(token)
+
         webhook_url = self.config['webhook']['webhook'] + 'queues/' + \
             str(self.player_data['queue']) + '/head/status/'
         kwargs = {
             'current_track': self.core.playback.current_track.get(),
-            'state': "stopped",
+            'state': 'stopped',
             'time_position': self.core.playback.time_position.get(),
         }
-        webhook.post(self.__class__.__name__, webhook_url, **kwargs)
+
+        self.webhook.post(self.__class__.__name__, webhook_url, **kwargs)
 
     def report_status(self):
-        token = self.config['webhook']['token']
-        webhook = Webhooks(token)
         webhook_url = self.config['webhook']['webhook'] + 'queues/' + \
             str(self.player_data['queue']) + '/head/status/'
         kwargs = {
+            'current_track': self.core.playback.current_track.get(),
             'state': self.core.playback.state.get(),
+            'time_position': self.core.playback.time_position.get(),
         }
-        webhook.post(self.__class__.__name__, webhook_url, **kwargs)
+
+        self.webhook.post(self.__class__.__name__, webhook_url, **kwargs)
+
+        # Loop back on itself every 2 seconds
         time.sleep(2)
         self.in_future.report_status()
 
 
 class EventReporter(pykka.ThreadingActor, CoreListener):
-
+    """Updates the API server each time an event is trigered by mopidy.
+    """
     def __init__(self, config, core, player_data):
         super(EventReporter, self).__init__()
+        self.core = core
         self.config = config
         self.player_data = player_data
+        self.time_position = None
+        self.webhook = Webhooks(config['webhook']['token'])
 
     def on_start(self):
         logger.info('{0} actor started.'.format(self.__class__.__name__))
-        pass
 
     def on_stop(self):
         logger.info('{0} actor stopped.'.format(self.__class__.__name__))
-        pass
 
     def on_event(self, event, **kwargs):
-        token = self.config['webhook']['token']
-        webhook = Webhooks(token)
-        webhook_url = self.config['webhook']['webhook'] + 'players/' + \
-            token + '/event/' + event + '/'
+        logger.info('{0} actor event: {1}.'.format(
+            self.__class__.__name__), event)
+
+        webhook_url = self.config['webhook']['webhook'] + 'queues/' + \
+            self.player_data['queue'] + '/head/events/' + event + '/'
         kwargs = {
             'event': event,
+            'args': kwargs
         }
-        webhook.post(self.__class__.__name__, webhook_url, **kwargs)
+
+        self.webhook.post(self.__class__.__name__, webhook_url, **kwargs)
