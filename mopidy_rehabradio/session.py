ae7cc019bf56a589806b0164cc08a14844c31de1
mark mcconnell <mark@rehabstudio.com>
2015-02-16 12:32:39 +0000

    sessions cleaned up

--- mopidy_rehabradio/session.py
+++ mopidy_rehabradio/session.py
@@ -14,43 +14,29 @@ logger = logging.getLogger(__name__)
 class WebhookSession(object):
 
     def __init__(self, config):
-        self.device = None
-        self.token = config['webhook']['token']
-        self.webhook = Webhooks(self.token)
+        self.webhook = Webhooks(config['webhook']['token'])
         self.base_url = config['webhook']['webhook']
+
+    def start(self):
+        logger.info('Webhook session started')
+
+    def stop(self):
+        logger.info('Session ended.')
 
     def fetch_head(self):
         logger.info('Fetching head track')
         webhook_url = '{0}queues/head/'.format(self.base_url)
 
-        track = self.webhook.get(self.__class__.__name__, webhook_url)
-        return track
+        return self.webhook.get(webhook_url)
 
     def pop_head(self, kwargs):
         logger.info('Removing current head track')
         webhook_url = '{0}queues/head/'.format(self.base_url)
 
-        next_track = self.webhook.delete(self.__class__.__name__, webhook_url, **kwargs)
-        return next_track
-
-    def start(self):
-        logger.info('Webhook session started')
-        webhook_url = '{0}players/'.format(self.base_url)
-
-        response = self.webhook.get(self.__class__.__name__, webhook_url)
-        self.device = response['results'][0]
-
-    def stop(self):
-        logger.info('Session ended.')
+        return self.webhook.delete(webhook_url, **kwargs)
 
     def update_head(self, kwargs):
         logger.info('Updating current head track.')
         webhook_url = '{0}queues/head/'.format(self.base_url)
 
-        self.webhook.patch(self.__class__.__name__, webhook_url, **kwargs)
-
-    def report_status(self, **kwargs):
-        pass
-
-    def report_event(self, **kwargs):
-        pass
+        return self.webhook.patch(webhook_url, **kwargs)
